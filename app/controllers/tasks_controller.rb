80dbd9d21d863d327af7ec601624d0dce604167d
Alexander <shkipersany@gmail.com>
2016-11-19 14:02:26 +0000

    Part 2

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+    def index
+        @task = Task.new
+        @tasks = Task.all.order(created_at: :desc)
+    end
+    
+    def create
+       @task = Task.new(tasks_params)
+       if @task.save
+           redirect_to root_path
+       else
+           redirect_to root_path
+       end
+       
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+      @task = Task.find(params[:id])
+     
+      if @task.update(tasks_params)
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
+    end
+    
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+     
+      redirect_to root_path
+    end
+    
+    private
+        def tasks_params
+            params.require(:task).permit(:title, :text)
+        end
+end
